ae82df150904d73ace81efb346aa0bc43a570212
Maxim Konoschenko <maksim08k@gmail.com>
2026-01-17 14:24:16 +0300

    added PZ_8

---
+++ PZ_8/PZ_8.1.py
@@ -0,0 +1,7 @@
+#Отсортируйте словарь {'Physics': 82, 'Math': 65, 'History': 75} по значению по возрастанию
+
+subjects = {'Physics': 82, 'Math': 65, 'History': 75}
+
+sorted_subjects = sorted(subjects.values())
+
+print(sorted_subjects)
