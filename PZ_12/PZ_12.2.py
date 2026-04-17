a8ff900862c543b51410117588e26ad604b419db
Maxim Konoschenko <maksim08k@gmail.com>
2026-04-17 15:47:55 +0300

    fixed

--- PZ_12/PZ_12.2.py
+++ PZ_12/PZ_12.2.py
@@ -1,7 +1,7 @@
 # В матрице наити сумму элементов первых двух строк.
 
-def sum_first_two_rows(matrix):
-    return sum(map(sum, matrix[:2])) if len(matrix) >= 2 else "У вас недостаточно строк, добавьте еще"
+def sum_two(matrix):
+    return sum(map(sum, matrix[:2]))
 
 test_matrix = [
     [1, 1, 1],
@@ -9,4 +9,4 @@ test_matrix = [
     [3, 3, 3]
 ]
 
-print(sum_first_two_rows(test_matrix))
+print(sum_two(test_matrix))
